f967da79c73d47b623092fad8c0691facb637b7f
sontran-q7 <anhsontran.q7@gmail.com>
2024-06-14 20:29:22 +0700

    Son
    menu_new

--- src/main/java/com/app/coffee/menu/totalInvoicePanel.java
+++ src/main/java/com/app/coffee/menu/totalInvoicePanel.java
@@ -6,14 +6,14 @@ package com.app.coffee.menu;
 
 /**
  *
- * @author anhso
+ * @author admin
  */
-public class test extends javax.swing.JPanel {
+public class totalInvoicePanel extends javax.swing.JPanel {
 
     /**
-     * Creates new form test
+     * Creates new form totalInvoicePanel
      */
-    public test() {
+    public totalInvoicePanel() {
         initComponents();
     }
 
@@ -26,16 +26,7 @@ public class test extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
     }// </editor-fold>//GEN-END:initComponents
 
 
